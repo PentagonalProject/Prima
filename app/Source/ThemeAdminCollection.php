9c5338c8c20fcd90d0a2fc27846cfa66de7439d5
pentagonal <pentagonaldevelopment@gmail.com>
2017-09-15 12:03:39 +0700

    Add Files for template admin

--- app/Source/ThemeAdminCollection.php
+++ app/Source/ThemeAdminCollection.php
@@ -14,12 +14,20 @@ class ThemeAdminCollection extends ThemeCollection
      */
     protected $mustBeExists = [
         self::FILE_INFO => self::INVALID_REASON_INFO_NOT_EXISTS,
-        '401.phtml' => self::INVALID_REASON_INCOMPLETE,
-        'index.phtml' => self::INVALID_REASON_INCOMPLETE,
-        'login.phtml' => self::INVALID_REASON_INCOMPLETE,
-        'users.phtml' => self::INVALID_REASON_INCOMPLETE,
-        'profile.phtml' => self::INVALID_REASON_INCOMPLETE,
-        'header.phtml' => self::INVALID_REASON_INCOMPLETE,
-        'footer.phtml' => self::INVALID_REASON_INCOMPLETE,
+        '401.phtml'     => self::INVALID_REASON_INCOMPLETE,
+        'index.phtml'   => self::INVALID_REASON_INCOMPLETE,
+        'header.phtml'  => self::INVALID_REASON_INCOMPLETE,
+        'footer.phtml'  => self::INVALID_REASON_INCOMPLETE,
+        'login.phtml'   => self::INVALID_REASON_INCOMPLETE,
+        'profile.phtml'  => self::INVALID_REASON_INCOMPLETE,
+        // manage
+        'manage/extensions.phtml' => self::INVALID_REASON_INCOMPLETE,
+        'manage/themes.phtml'   => self::INVALID_REASON_INCOMPLETE,
+        'manage/posts.phtml'    => self::INVALID_REASON_INCOMPLETE,
+        'manage/settings.phtml'    => self::INVALID_REASON_INCOMPLETE,
+        'manage/users.phtml'    => self::INVALID_REASON_INCOMPLETE,
+        // edit
+        'edit/post.phtml'    => self::INVALID_REASON_INCOMPLETE,
+        'edit/user.phtml'    => self::INVALID_REASON_INCOMPLETE,
     ];
 }
